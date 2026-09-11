336b3d6e7b82371938b7d6a15e33a6f14c635393
soroush MohamadKhani <soroush138388@gmail.com>
2026-09-11 16:48:23 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from models import db, User, Product, Warehouse, Supplier, Stock, Transaction
+from models import db, User, Product, Warehouse, Supplier, Stock, Transaction, Vehicle, Driver, ShipmentRoute
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///enterprise_supply_chain.db'
@@ -26,10 +27,26 @@ def dashboard():
         t_count=transactions_count,
         transactions=recent_transactions
     )
+
+
 @app.route('/products')
 def list_products():
     products = Product.query.all()
     return render_template('products.html', products=products)
+
+
+@app.route('/vehicles')
+def list_vehicles():
+    vehicles = Vehicle.query.all()
+    drivers = Driver.query.all()
+    return render_template('vehicles.html', vehicles=vehicles, drivers=drivers)
+
+
+@app.route('/routes')
+def list_routes():
+    routes = ShipmentRoute.query.all()
+    return render_template('routes.html', routes=routes)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
